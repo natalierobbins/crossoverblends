13aba5d28de1b52356caa3f6977aae9ddf061677
robbinat <robbinat@umich.edu>
2023-09-27 13:48:35 -0400

    landing responsive

--- src/components/venn.js
+++ src/components/venn.js
@@ -1,6 +1,6 @@
 export const Venn = ({type}) => {
     return (
-        <div className='venn-wrapper -flex -row'>
+        <div className='venn-wrapper -flex -al-c -jc-c'>
             <span className={`venn-circle apple ${type}`}></span>
             <span className={`venn-circle spotify ${type}`}></span>
         </div>
